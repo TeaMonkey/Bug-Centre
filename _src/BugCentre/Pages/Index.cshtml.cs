aabb0b54bf1b9149d1038e51cc568048c04c9adf
TeaMonkey <TeaMonkey@users.noreply.github.com>
2019-12-08 15:47:03 +0000

    Initial commit 2

---
+++ _src/BugCentre/Pages/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace BugCentre.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+
+        public string Username {get;set;}
+
+  
+        public string Password {get; set;}
+
+        public string Msg {get; set;}
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //Can this be nullable?
+            if (Username.Equals("bob") && Password.Equals("123"))
+            {
+                HttpContext.Session.SetString("username", Username);
+                return RedirectToPage("Welcome");
+            }
+            else
+            {
+                Msg = "Incorrect username and/or password";
+                return Page();
+            }
+        }
+    }
+}
---
+++ _src/BugCentre/Pages/Welcome.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+namespace MyApp.Namespace
+{
+    public class WelcomeModel : PageModel
+    {
+        public string Username { get; set; }
+        
+        public void OnGet()
+        {
+            Username = HttpContext.Session.GetString("username");
+        }
+
+        public IActionResult OnGetLogout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToPage("Index");
+        }
+    }
+}
